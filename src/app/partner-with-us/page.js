925c29215cff97a34afeaff9e57e775cf575bbd9
souravbsk <souravbsk01@gmail.com>
2023-06-18 21:52:34 +0600

    code change

---
+++ src/app/partner-with-us/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PartnerWithUs = () => {
+    return (
+        <div>
+            <h1>PartnerWithUs</h1>
+        </div>
+    );
+};
+
+export default PartnerWithUs;
